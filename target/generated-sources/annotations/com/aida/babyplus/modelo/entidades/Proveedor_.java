37f54e5f6fca6b4b2144dee80d8f2c918bb8fed9
Aida <Aidacav@github.com>
2021-11-28 20:39:21 +0100

    Cambiar ejecuciones

--- target/generated-sources/annotations/com/aida/babyplus/modelo/entidades/Proveedor_.java
+++ target/generated-sources/annotations/com/aida/babyplus/modelo/entidades/Proveedor_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-11-28T19:55:06")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-11-28T20:38:15")
 @StaticMetamodel(Proveedor.class)
 public class Proveedor_ { 
 
--- target/generated-sources/annotations/com/aida/babyplus/modelo/entidades/Usuario_.java
+++ target/generated-sources/annotations/com/aida/babyplus/modelo/entidades/Usuario_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-11-28T19:55:06")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-11-28T20:38:15")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
